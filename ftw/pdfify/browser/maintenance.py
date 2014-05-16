fab6a969c558921067906d4ab54408758a5f661a
Philippe Gross <philippe.gross@4teamwork.ch>
2014-05-16 17:07:57 +0200

    Added maintenance view.

--- ftw/pdfify/browser/maintenance.py
+++ ftw/pdfify/browser/maintenance.py
@@ -5,7 +5,14 @@ from zope.publisher.browser import BrowserView
 class PdfifyMaintenanceView(BrowserView):
 
     def start_pdf_conversion(self):
+        """Starts the pdf conversion for the current context."""
+
         pdfier = IPdf(self.context)
         pdfier.convert_to_pdf()
 
         return pdfier.storage.token
+
+    def pdf(self):
+        """Returns the pdf for the saved context."""
+
+        return IPdf(self.context).pdf_download()
